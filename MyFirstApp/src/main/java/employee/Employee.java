904bdaf2e5e47b0a38e20812ccac7285b9590d77
Noorhn-Khaled <100763814+Noorhan-Khaled@users.noreply.github.com>
2022-03-05 22:48:34 +0200

    enter data of class

--- MyFirstApp/src/main/java/employee/Employee.java
+++ MyFirstApp/src/main/java/employee/Employee.java
@@ -1,9 +1,41 @@
 package employee;
 
 public class Employee {
-    int empId;
+    int employeeId;
     String name;
     int salary;
 
+    Employee(int employeeId,String name,int salary){
+        this.employeeId=employeeId;
+        this.name=name;
+        this.salary=salary;
 
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    public void print(){
+        System.out.print(getEmployeeId()+" "+getName()+" "+getSalary());
+    }
 }
