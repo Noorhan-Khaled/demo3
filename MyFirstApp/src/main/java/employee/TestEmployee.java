d804173620ebce74f55dce28f674756eda08d4d1
Noorhn-Khaled <100763814+Noorhan-Khaled@users.noreply.github.com>
2022-03-05 22:56:49 +0200

    test employee

---
+++ MyFirstApp/src/main/java/employee/TestEmployee.java
@@ -0,0 +1,20 @@
+package employee;
+
+import employee.Employee;
+
+import java.util.ArrayList;
+
+public class TestEmployee {
+    public static void main(String[] args) {
+        ArrayList<Employee> list=new ArrayList<Employee>();
+        for (int i=1;i<=10;i++){
+            Employee employee=new Employee(i,Integer.toString(i),i);
+            list.add(employee);
+        }
+        for (int i=0;i< list.size();i++){
+            Employee employee=list.get(i);
+            employee.print();
+        }
+
+    }
+}
