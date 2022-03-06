f24f313d8c912f7c03b08de32a487ab17579dd92
Noorhn-Khaled <100763814+Noorhan-Khaled@users.noreply.github.com>
2022-03-06 12:18:48 +0200

    add text file

--- MyFirstApp/src/main/java/employee/TestEmployee.java
+++ MyFirstApp/src/main/java/employee/TestEmployee.java
@@ -2,10 +2,14 @@ package employee;
 
 import employee.Employee;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class TestEmployee {
     public static void main(String[] args) {
+
+        File file=new File("D:\\informatique\\work\\arr\\demo3\\MyFirstApp\\src\\main\\java\\employee\\employee");
+
         ArrayList<Employee> list=new ArrayList<Employee>();
         for (int i=1;i<=10;i++){
             Employee employee=new Employee(i);
